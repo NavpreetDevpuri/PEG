331c1ea9f2e424bd21e824678b440adf9ae22764
nitin_tejuja <nitin.tejuja@1silverbullet.tech>
2021-10-24 13:10:57 +0530

    implemented 3 methods

--- peg/main.py
+++ peg/main.py
@@ -19,17 +19,27 @@ class Peg:
         return screenshot_name_to_path_map
 
     def open_run_command_window(self):
-        pyautogui.hotkey('win','r')
+        pyautogui.hotkey('win', 'r')
 
-    def open_file_or_folder_from_run_command_window(self, file_or_folder_path):
-        pyautogui.hotkey('win','r')
+    def open_file_or_folder_from_run_command_window(self,file_or_folder_path):
+        # open run window
+        pyautogui.hotkey('win', 'r')
+        # write path in run window
         pyautogui.write(file_or_folder_path)
+        # press enter so that we go to that  path
         pyautogui.hotkey('enter')
 
-    def download_file_from_url_using_chrome(self, file_url):
-        pyautogui.hotkey('win','r')
+    def download_file_from_url_using_chrome(self,file_url):
+        pyautogui.hotkey('win', 'r')
         pyautogui.write('chrome')
-        pyautogui.hotkey('alt','d')
+        # open chrome
+        pyautogui.hotkey('enter', interval=3)
+        # We got to enter url section
+        pyautogui.hotkey('alt', 'd')
+        # paste the given file url so that we can download the file
         pyautogui.write(file_url)
+        # let file load first then we download
+        pyautogui.hotkey('enter', interval=3)
+        pyautogui.hotkey('ctrl', 's', interval=1)
+        # if window is there then we have to press enter button to download file
         pyautogui.hotkey('enter')
-
